be55a0e2e7d9ef9553d59b5c711f40ea406c01f5
Ahmed Eldeeb <eldeeb757575@gmail.com>
2024-05-30 16:51:56 +0300

    Styling Iussues

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,31 +1,33 @@
 #!/usr/bin/python3
 """
-  Pascal's Triangle
+Pascal's Triangle
 """
-def pascal_triangle(n: int) -> list[list[int]]:
-  """
-  Generate Pascal's triangle up to a given number of rows.
 
-  Args:
+
+def pascal_triangle(n: int) -> list[list[int]]:
+    """
+    Generate Pascal's triangle up to a given number of rows.
+
+    Args:
     n (int): The number of rows to generate in the triangle.
 
-  Returns:
+    Returns:
     List[List[int]]: A list of lists representing Pascal's triangle.
 
-  """
+    """
 
-  if (n <= 0):
-    return []
+    if (n <= 0):
+        return []
 
-  pascal = []
-  pascal.append([1])
+    pascal = []
+    pascal.append([1])
 
-  for i in range(1, n):
-    rowList = [1]*(i+1)
-    
+    for i in range(1, n):
+        rowList = [1]*(i+1)
+
     for j in range(1, i):
-      rowList[j] = pascal[i-1][j-1] + pascal[i-1][j]
-    
+        rowList[j] = pascal[i-1][j-1] + pascal[i-1][j]
+
     pascal.append(rowList)
 
-  return pascal
+    return pascal
